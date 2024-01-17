4ab4322790d950a12e812ca1c11bfec248b023fe
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-01-17 11:18:45 -0300

    layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
-export default function Layout() {
+export default function Layout({ children }) {
   return (
     <>
       <header>
         <h1>PetShop</h1>
         <nav>
           <Link href="/">Blog</Link>
-          <Link href="/">Produtos</Link>
-          <Link href="/">Sobre</Link>
-          <Link href="/">Contato</Link>
+          <Link href="/produtos">Produtos</Link>
+          <Link href="/sobre">Sobre</Link>
+          <Link href="/contato">Contato</Link>
         </nav>
       </header>
+      <main className="limitador">{children}</main>
     </>
   );
 }
