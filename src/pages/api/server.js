5b2d5268e562438e2b81648bfd39b69f2e6c7840
@Nicolas_Oliveira <125362779+Nicolas-tech25@users.noreply.github.com>
2024-01-30 09:36:36 -0300

    sever alterado
    colocando nova api

--- src/pages/api/server.js
+++ src/pages/api/server.js
@@ -1,5 +1,2 @@
-const porta = 2112;
-const endereco = "10.20.46.26";
-
-const serverApi = `http://${endereco}:${porta}`;
+const serverApi = `https://petshop-nicolas-default-rtdb.firebaseio.com/`;
 export default serverApi;
